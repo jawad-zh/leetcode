3b1d31f838c6a78e162aedce58fb2fa501a8da04
jawad-zh <zahraoui.jawad20@gmail.com>
2025-09-26 11:47:39 +0100

    do

--- ValidParentheses/main.go
+++ ValidParentheses/main.go
@@ -3,41 +3,50 @@ package main
 import "fmt"
 
 func main() {
-	Result := isValid("({})")
+	Result := isValid("({)}")
 	fmt.Println(Result)
 }
 
 func isValid(s string) bool {
-	var RoundBrackets int
-	var SquareBrackets int
-	var CurlyBrackets int
-	for i := 0; i < len(s); i++ {
-		if s[i] == '(' {
-			RoundBrackets++
-			}else if s[i] == ')' {
-				RoundBrackets--
-				if (CurlyBrackets != 0 || SquareBrackets != 0) && RoundBrackets!=0 {
+	var Result bool
+	var RoundOver bool 
+	var CurlyOver bool 
+	var SquarOver bool 
+	var RoundIndex int 
+	var CurlyIndex int 
+	var SquarIndex int 
+	for i:=0 ; i < len(s) ; i++{
+		for j:=i+1 ; j < len(s) ; j++{
+		if s[i]== '(' {
+			RoundOver = true 
+			RoundIndex = i
+			 if s[j]== ')'{
+			if (CurlyOver == true || SquarOver == true ) && (RoundIndex < CurlyIndex || RoundIndex < SquarIndex)  {
+				fmt.Println(Result)
 				return false
 			}
-			} else if s[i] == '{' {
-				CurlyBrackets++
-			} else if s[i] == '}' {
-				CurlyBrackets--
-					if (RoundBrackets != 0 || SquareBrackets != 0) && CurlyBrackets !=0 {
-					return false
-				}
-			} else if s[i] == '[' {
-				
-				SquareBrackets++
-			} else if s[i] == ']' {
-				SquareBrackets--
-				if (RoundBrackets != 0 || CurlyBrackets != 0) && SquareBrackets!=0 {
-					return false
-				}
+		}
+		}else if s[i]== '{' {
+			CurlyOver = true 
+			CurlyIndex = i
+			 if s[j]== '}'{
+			if (RoundOver == true || SquarOver == true ) && (CurlyIndex < RoundIndex || CurlyIndex < SquarIndex)  {
+				fmt.Println(Result)
+				return false
 			}
 		}
-		if RoundBrackets == 0 && SquareBrackets == 0 && CurlyBrackets == 0 {
-			return true
+		}else if s[i]== '[' {
+			SquarOver = true 
+			SquarIndex = i
+			 if s[j]== ']'{
+			if (CurlyOver == true || RoundOver == true ) && (SquarIndex < CurlyIndex || SquarIndex < RoundIndex)  {
+				fmt.Println(Result)
+				return false
+			}
 		}
-		return false
+		}
 	}
+	}
+	// fmt.Println(true)
+	return true
+}
